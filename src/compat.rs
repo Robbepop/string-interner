20a74edb8128ef1f193a038f81a916c3a7e34bfe
Robin Freyler <robin.freyler@gmail.com>
2020-07-12 15:37:16 +0200

    add no_std compatibility module

---
+++ src/compat.rs
@@ -0,0 +1,44 @@
+//! Compatibility layer for `no_std` compilations.
+
+use cfg_if::cfg_if;
+
+cfg_if! {
+    if #[cfg(all(not(feature = "std"), not(feature = "hashbrown")))] {
+        // If the compile for `no_std` we need to enable `hashbrown`.
+        compile_error!(
+            "encountered invalid set of crate features. use `std` or `no_std` + `hashbrown`"
+        )
+    }
+}
+
+cfg_if! {
+    if #[cfg(feature = "hashbrown")] {
+        pub use ::hashbrown::HashMap;
+        pub use ::hashbrown::hash_map::DefaultHashBuilder;
+    } else {
+        pub use ::std::collections::HashMap;
+    }
+}
+
+cfg_if! {
+    if #[cfg(feature = "std")] {
+        pub use ::std::{
+            collections::{
+                hash_map::RandomState,
+                hash_map::RandomState as DefaultHashBuilder,
+            },
+            vec,
+            vec::Vec,
+            string::String,
+            boxed::Box,
+        };
+    } else {
+        extern crate alloc;
+        pub use self::alloc::{
+            vec,
+            vec::Vec,
+            string::String,
+            boxed::Box,
+        };
+    }
+}
