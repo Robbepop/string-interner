ea339118b193f953d3289cb242ffbfb74f1fc26e
Robin Freyler <robin.freyler@gmail.com>
2020-07-14 11:56:26 +0200

    make hashbrown dependency mandatory
    All benchmarks show ~20-30% performance improvement.
    Also this will allow us access to the raw_entry API.

--- src/compat.rs
+++ src/compat.rs
@@ -2,31 +2,15 @@
 
 use cfg_if::cfg_if;
 
-cfg_if! {
-    if #[cfg(all(not(feature = "std"), not(feature = "hashbrown")))] {
-        // If the compile for `no_std` we need to enable `hashbrown`.
-        compile_error!(
-            "encountered invalid set of crate features. use `std` or `no_std` + `hashbrown`"
-        )
-    }
-}
-
-cfg_if! {
-    if #[cfg(feature = "hashbrown")] {
-        pub use ::hashbrown::HashMap;
-        pub use ::hashbrown::hash_map::DefaultHashBuilder;
-    } else {
-        pub use ::std::collections::HashMap;
-    }
-}
+pub use ::hashbrown as hash_map;
+pub use ::hashbrown::{
+    hash_map::DefaultHashBuilder,
+    HashMap,
+};
 
 cfg_if! {
     if #[cfg(feature = "std")] {
         pub use ::std::{
-            collections::{
-                hash_map::RandomState,
-                hash_map::RandomState as DefaultHashBuilder,
-            },
             vec,
             vec::Vec,
             string::{String, ToString},
