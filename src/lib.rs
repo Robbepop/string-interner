f304dd9f8135af9e06c7a6ad3cd02ba21e122f14
Robin Freyler <robin.freyler@gmail.com>
2020-07-15 03:06:35 +0200

    bump version 0.11.2 -> 0.11.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/crate/string-interner/0.11.2")]
+#![doc(html_root_url = "https://docs.rs/crate/string-interner/0.11.3")]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![deny(missing_docs)]
 
