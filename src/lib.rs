f22ff603b0923f4942a13676be300c4cf1b24f8d
Robin Freyler <robin.freyler@gmail.com>
2024-11-12 22:48:38 +0100

    remove commented out code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![doc(html_root_url = "https://docs.rs/crate/string-interner/0.12.0")]
 #![cfg_attr(not(feature = "std"), no_std)]
-// #![deny(missing_docs)]
 #![warn(unsafe_op_in_unsafe_fn, clippy::redundant_closure_for_method_calls)]
 
 //! Caches strings efficiently, with minimal memory footprint and associates them with unique symbols.
